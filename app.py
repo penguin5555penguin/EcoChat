95d024c6cb9f81136a7b5d089ee39cd5a1bdde48
penguin5555penguin <aaravdeshmanealt@gmail.com>
2024-05-29 15:57:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -621,4 +621,4 @@ def handleMessage(msg):
     
 if __name__ == '__main__':
     # socketio.run(app, host='localhost', port=5000)
-    socketio.run(app, host='192.168.0.87', port=5000)    
+    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)   
