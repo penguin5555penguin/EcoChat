6aa2cc467c22ba8d223f48535bdfb45556131cd4
penguin5555penguin <aaravdeshmanealt@gmail.com>
2024-05-29 18:39:40 -0700

    Eco Chat v.1.0

--- app.py
+++ app.py
@@ -621,4 +621,4 @@ def handleMessage(msg):
     
 if __name__ == '__main__':
     # socketio.run(app, host='localhost', port=5000)
-    socketio.run(app, host='0.0.0.0', port=5000, allow_unsafe_werkzeug=True)   
+    socketio.run(app, host='0.0.0.0', port=10000, allow_unsafe_werkzeug=True)   
